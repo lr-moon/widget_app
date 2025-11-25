c3d177d4c6c78e51b285524d1f63009dd3784a27
lr-moon <monselopezrubio3@gmail.com>
2025-11-24 21:30:48 -0600

    Cards

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -25,150 +25,125 @@ class _ButtonsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding:
-            const EdgeInsets.symmetric //Aplica espacio en los lados contrarios (ejempplo, espacio de derecha a izquierda)
-            (horizontal: 10, vertical: 20),
-        //Wrap, Cuando ya no hay espacio, se salta a la siguiente linea
-        child: Wrap(
-          crossAxisAlignment: WrapCrossAlignment.center,
-          spacing: 17,
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Boton elevado con sombra'),
+    //Theme.of para que busque lo mas cercano a el
+    final colors = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      //cuando ya no hay espacios
+      child: Wrap(
+        spacing: 18,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Boton elevado con sombra'),
+          ),
+          const ElevatedButton(
+            onPressed: null,
+            child: Text('Boton deshabilitado'),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.add_home_outlined),
+            label: const Text('Eleveted icon'),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.star),
+            label: const Text("Elevated Icon"),
+          ),
+          FilledButton(onPressed: () {}, child: const Text("Filled")),
+          FilledButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.check_circle_outline),
+            label: const Text("Filled Icon"),
+          ),
+          const SizedBox(width: 10),
+          OutlinedButton(onPressed: () {}, child: const Text("Outline")),
+          OutlinedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.info_outline),
+            label: const Text("Outline Icon"),
+          ),
+          const SizedBox(width: 10),
+          TextButton(onPressed: () {}, child: const Text("TextB")),
+          ElevatedButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 201, 216, 229),
+              foregroundColor: Colors.purple,
             ),
+            icon: const Icon(Icons.save),
+            label: const Text('Guardar cambios'),
+          ),
 
-            ElevatedButton(onPressed: null, child: Text('Boton deshabilitado')),
-
-            ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.account_box_outlined),
-              label: const Text('Elevated icon'),
+          TextButton.icon(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 239, 231, 172),
+              foregroundColor: const Color.fromARGB(255, 0, 0, 0),
             ),
-
-            // 1. Botón con colores personalizados (Fondo y Texto)
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 85, 51, 206),
-                foregroundColor: Colors.white,
-              ),
-              child: const Text('Fondo y texto'),
+            icon: const Icon(Icons.download),
+            label: const Text('Descargar'),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.calendar_today_outlined),
+            label: const Text('Calendario'),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.coffee_maker),
+            label: const Text('cafetera'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+              padding: const EdgeInsets.all(15),
             ),
-
-            // Botón sin sombra (Sin Elevación)
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                shadowColor: Colors.transparent,
-              ),
-              child: const Text('Sin elevación (Plano)'),
+            child: const Icon(Icons.apps),
+          ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.add_moderator_outlined),
+            label: const Text('TextB Icon'),
+          ),
+          const CustomButton(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.app_registration_rounded),
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(colors.primary),
+              iconColor: MaterialStatePropertyAll(Colors.white),
             ),
+            //WidgetStatePropertyAll :permite un estado a nuestro color que vamos elegir en su momento
+            //ButtonStyle: darle estilo a los botones
+            //iconColor:darel un color a mi icono
+            //MaterialStatePropertyAll:darle una propiedad a nuestro icon
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-            // 4. Botón con borde de color
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                side: const BorderSide(
-                  color: Color.fromARGB(255, 122, 21, 184),
-                  width: 2,
-                ),
-              ),
-              child: const Text('Borde de color'),
-            ),
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
 
-            // Botón cuadrado (Sin radio de borde)
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero,
-                ),
-              ),
-              child: const Text('Hola Mundo con Flutter'),
-            ),
-
-            // ElevatedButton.icon con color de fondo diferente
-            ElevatedButton.icon(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                foregroundColor: Colors.white,
-              ),
-              icon: const Icon(Icons.save),
-              label: const Text('Guardar cambios'),
-            ),
-
-            // Botón totalmente circular
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(20),
-                backgroundColor: const Color.fromARGB(255, 221, 231, 240),
-              ),
-              child: const Icon(Icons.add),
-            ),
-
-            // Botón con sombra de color intenso (Efecto neón/glow)
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                elevation: 15,
-                shadowColor: const Color.fromARGB(255, 56, 54, 55),
-              ),
-              child: const Text('Sombra Intensa'),
-            ),
-
-            // Botón con mucho padding interno (Más espacioso)
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 20,
-                ),
-              ),
-              child: const Text('Padding Ampliado'),
-            ),
-
-            // TextButton, bbotón plano)
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.indigo, // Color del texto y del splash
-                textStyle: const TextStyle(fontSize: 16),
-              ),
-              child: const Text('Boton de solo Texto'),
-            ),
-
-            // TextButton.icon (Texto + Icono sin fondo)
-            TextButton.icon(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                foregroundColor: const Color.fromARGB(255, 105, 7, 134),
-              ),
-              icon: const Icon(Icons.download),
-              label: const Text('Descargar aqui'),
-            ),
-
-            // Botón con icono a la derecha (Usando Row manualmente)
-            ElevatedButton(
-              onPressed: () {},
-              child: const Row(
-                mainAxisSize: MainAxisSize.min, // Ajusta el Row al contenido
-                children: [
-                  Text('Siguiente'),
-                  SizedBox(width: 8),
-                  Icon(Icons.arrow_forward_ios, size: 16),
-                ],
-              ),
-            ),
-          ],
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    //ClipRRect:que recorte y crea los bordes
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola', style: TextStyle(color: Colors.white)),
+          ),
         ),
       ),
     );
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,13 +1,211 @@
 import 'package:flutter/material.dart';
+
+const cards = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
+
 class CardsScreen extends StatelessWidget {
-    static const String name ="cards_screen";
+  static const String name = "cards_screen";
   const CardsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Cards Screen'),),
-      body: const Placeholder(),
+      appBar: AppBar(title: const Text('Cards Screen')),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //map;coventir un cards a otro tipo de mapa
+          ...cards.map(
+            (card) =>
+                _CardType1(label: card['label'], elevation: card['elevation']),
+          ),
+          ...cards.map(
+            (card) =>
+                _CardType2(label: card['label'], elevation: card['elevation']),
+          ),
+          ...cards.map(
+            (card) =>
+                _CardType3(label: card['label'], elevation: card['elevation']),
+          ),
+          ...cards.map(
+            (card) =>
+                _CardType4(label: card['label'], elevation: card['elevation']),
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  // ignore: unused_element_parameter
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      //EdgeInsets.fromLTRB :permite dar espacios diferentes a cada uno de ellos
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(alignment: Alignment.bottomLeft, child: Text(label)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  // ignore: unused_element_parameter
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      //shape:darle forma al boton
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: colors.outline),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      //side:darle border
+      elevation: elevation,
+      //EdgeInsets.fromLTRB :permite dar espacios diferentes a cada uno de ellos
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  // ignore: unused_element_parameter
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      //surfaceVariant:que se vea de la paleta de colores pero que se vea diferente son como coores pasteles
+      // ignore: deprecated_member_use
+      color: colors.surfaceVariant,
+      elevation: elevation,
+      //EdgeInsets.fromLTRB :permite dar espacios diferentes a cada uno de ellos
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - filled'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  // ignore: unused_element_parameter
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      //clipBehavior:permite identificar que se va a hacer si se desborda
+      //Clip.hardEdge:permite que los hijos no se salgan de su padre
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      //EdgeInsets.fromLTRB :permite dar espacios diferentes a cada uno de ellos
+      child: Stack(
+        children: [
+          //Image.network:imagenes de internet
+          //fit:que forma queremos
+          Image.network(
+            'https://picsum.photos/id/600/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.more_vert_outlined),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
